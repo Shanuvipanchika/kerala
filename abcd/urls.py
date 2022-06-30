cf1efb11e80b8504b8525e90eae28c2e7bc1f5e7
shanu <shanua8470@gmail.com>
2022-06-29 21:20:08 -0700

    first commit

---
+++ abcd/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from.import views
+urlpatterns = [
+    path("xyz",views.efg),
+    path("login",views.abcd,name='log-in'),
+    path("home",views.first,name='ho-me'),
+    path("new",views.new,name='new')
+]
---
+++ abcd/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# Create your views here.
+def efg(request):
+    return HttpResponse("hello friends")
+def abcd(request):
+    return render(request,"abcd/abcd.html")
+
+
+def first(request):
+    return render(request,"abcd/first.html")
+
+def new(request):
+    return render(request,"abcd/new.html")
